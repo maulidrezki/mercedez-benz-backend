4946afea116a85e97c7ee67c4d879301fa07914b
Maulid Rezki <maulidrezki13@gmail.com>
2022-01-10 12:59:42 +0700

    add mapbox api on museums controller

--- app/controllers/museums_controller.rb
+++ app/controllers/museums_controller.rb
@@ -1,4 +1,17 @@
+require 'json'
+require 'open-uri'
+
+def mapbox_apiurl(lat, lng)
+  "https://api.mapbox.com/geocoding/v5/mapbox.places/museum.json?type=poi&proximity=#{lng},#{lat}&access_token=#{ENV['MAPBOX_API_KEY']}"
+end
+
 class MuseumsController < ApplicationController
   def index
+    @lat = params[:lat]
+    @lng = params[:lng]
+    unless (@lat.nil? && @lng.nil?) || !(@lat.to_i.between?(-90, 90) && @lng.to_i.between?(-90, 90))
+      query = JSON.parse(URI.open(mapbox_apiurl(@lat, @lng)).read)['features'].map { |f| [f['context'][0]['text'], f['text']] }
+      @museums = query.inject(Hash.new{ |h,k| h[k]=[] }){ |h,(k,v)| h[k] << v; h }
+    end
   end
 end
